7986b68699b04635d0955de3cab4a4922595dc96
Guilherme Valarini <guilherme.a.valarini@gmail.com>
2022-10-17 12:26:42 -0300

    [OpenMP] Add distributed plugin based on MPI
    This patch adds a new device plugin that is able to distribute work to a cluster using MPI as its underlying communication layer. The plugin is entirely based on the paper "The OpenMP Cluster Programming Model" (The OpenMP Cluster Programming Model).
    
    Differential Revision: https://reviews.llvm.org/D136278

--- openmp/libomptarget/plugins/mpi/include/Common.h
+++ openmp/libomptarget/plugins/mpi/include/Common.h
@@ -1,4 +1,4 @@
-//===------- coroutines.h - Concurrent MPI communicaiton ------*- C++ -*-===//
+//===------------- coroutines.h - Common definitions ------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
