f93eaf0c0860d7455ae4b52a672e654272fc6f29
Guilherme Valarini <guilherme.a.valarini@gmail.com>
2022-10-17 12:26:42 -0300

    Fix typos

--- openmp/libomptarget/plugins/mpi/include/EventSystem.h
+++ openmp/libomptarget/plugins/mpi/include/EventSystem.h
@@ -1,4 +1,4 @@
-//===------- event_system.h - Concurrent MPI communicaiton ------*- C++ -*-===//
+//===------- event_system.h - Concurrent MPI communication ------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
